85645a8d8b3727694b614edbf6dd44d68e8b51f8
sakshi1195 <sakshipanchal1195@gmail.com>
2025-10-30 08:10:12 +0000

    coversion operations chapter 3

---
+++ 01_basis/conversionOperations.js
@@ -0,0 +1,32 @@
+let num = "333ab";
+ let valueofnum = Number(num);
+// console.log(typeof num);
+// console.log(typeof (num));
+// console.log(typeof valueofnum);
+// console.log(valueofnum);
+
+
+// "33 => 33
+// "331bc" => NaN - not a number
+// true => 1
+// false => 0
+// null => 0
+// undefined => NaN
+
+let loggedIn = 1;
+let booleanloggedIn = Boolean(loggedIn);
+// console.log(typeof booleanloggedIn);
+// console.log(booleanloggedIn);
+
+// 0 => false
+// 1 => true
+// "" => false
+// "sakshi" => true
+// null => false
+// undefined => false
+// NaN => false
+
+let str = 1234;
+let stringstr = String(str);
+console.log(typeof stringstr);
+console.log(stringstr);
